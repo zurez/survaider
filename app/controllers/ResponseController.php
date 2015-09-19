33bc9fd5e4752559693de5db8e8bce086a04d297
zurez <zurez4u@gmail.com>
2015-09-19 11:56:32 +0530

    ResponseController

---
+++ app/controllers/ResponseController.php
@@ -0,0 +1,39 @@
+<?php
+
+class ResponseController extends \BaseController {
+
+	public function filter()
+	{
+		$j = Input::get('jdata');
+		if ($j==null) {
+			return Response::json(array('message'=>'No Data Was Sent To Server'));
+		}
+		else{
+			$this->checkToken($j);
+		}
+	}
+	public function checkToken($j)
+	{
+		$json = json_decode($j,true);
+				if (Schema::hasTable($json['survey_id'])) {
+					# code..
+					//Not checking for token right now.
+					$this->saveResponse($json);
+				}
+				else {
+					return Response::json(array('message'=>'The Survey You Requested Does not Exists'));
+				}
+	}
+	public function tempStore($json)
+	{
+		
+	}
+	public function saveResponse($json)
+	{	$table= $json['survey_id'];
+		$cid= $json['c_id'];//Our Column
+		$response=$json['answer'];
+		$user= $json['u_id'];
+		DB::table($table)->insert(array($cid=>$response))
+	}
+
+}
